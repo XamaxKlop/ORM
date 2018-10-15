0b9501213d955519fb1f905e0eeb71e7f467fe88
XamaxKlop <44174982+XamaxKlop@users.noreply.github.com>
2018-10-15 21:12:00 +0300

    Revert "Second_test"
    This reverts commit 358cb835c02e63fc7604452036cdaf2c2aa72c86.

--- ORM_TEST/ORM_TEST/Program.cs
+++ ORM_TEST/ORM_TEST/Program.cs
@@ -3,3 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+namespace ORM_TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
